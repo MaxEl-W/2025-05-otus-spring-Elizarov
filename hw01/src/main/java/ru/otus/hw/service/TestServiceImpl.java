9afdd784eb8e6af878a92f6ed06f052b18fe2cab
m.elizarov <maxel5g@gmail.com>
2025-10-26 21:53:26 +0300

    реализация сервиса тестирования Unit тесты Добавлени вопросов Исправление зависимостей

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,45 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
-
     private final IOService ioService;
+
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+
+        List<Question> questions = questionDao.findAll();
+        printQuestions(questions);
+    }
+
+    private void printQuestions(List<Question> questions) {
+        QuestionScreenConverter converter = new QuestionScreenConverter();
+        for (Question question : questions) {
+            String questionText = converter.toString(question);
+            ioService.printLine(questionText);
+        }
+    }
+
+    public static class QuestionScreenConverter {
+        public String toString(Question question) {
+            StringBuilder sb = new StringBuilder(question.text());
+            sb.append("\n");
+            for (Answer answer : question.answers()) {
+                sb.append("\t");
+                sb.append(answer.text());
+                sb.append("\n");
+            }
+            return sb.toString();
+        }
     }
 }
