3159acb4ff5e2de2a423ef42a2b58db2695848f7
m.elizarov <m.elizarov@argustelecom.ru>
2025-10-29 18:04:22 +0300

    Revert "реализация сервиса тестирования"
    This reverts commit 9afdd784eb8e6af878a92f6ed06f052b18fe2cab.

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,45 +1,16 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import ru.otus.hw.dao.QuestionDao;
-import ru.otus.hw.domain.Answer;
-import ru.otus.hw.domain.Question;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
-    private final IOService ioService;
 
-    private final QuestionDao questionDao;
+    private final IOService ioService;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-
-        List<Question> questions = questionDao.findAll();
-        printQuestions(questions);
-    }
-
-    private void printQuestions(List<Question> questions) {
-        QuestionScreenConverter converter = new QuestionScreenConverter();
-        for (Question question : questions) {
-            String questionText = converter.toString(question);
-            ioService.printLine(questionText);
-        }
-    }
-
-    public static class QuestionScreenConverter {
-        public String toString(Question question) {
-            StringBuilder sb = new StringBuilder(question.text());
-            sb.append("\n");
-            for (Answer answer : question.answers()) {
-                sb.append("\t");
-                sb.append(answer.text());
-                sb.append("\n");
-            }
-            return sb.toString();
-        }
+        // Получить вопросы из дао и вывести их с вариантами ответов
     }
 }
