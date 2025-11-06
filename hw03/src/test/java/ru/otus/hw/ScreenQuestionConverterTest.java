038ecd1715bf9a65056136ac602c00876b443558
m.elizarov <maxel5g@gmail.com>
2025-11-06 07:35:50 +0300

    ADD tests

---
+++ hw03/src/test/java/ru/otus/hw/ScreenQuestionConverterTest.java
@@ -0,0 +1,30 @@
+package ru.otus.hw;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.service.ScreenQuestionConverter;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith({MockitoExtension.class})
+public class ScreenQuestionConverterTest {
+    @InjectMocks
+    private ScreenQuestionConverter converter;
+
+    @Test
+    public void shouldValidFormat() {
+        var expectedQuestionFormatedText = "Question\n" + String.format("\t1. %s%n\t2. %s%n", "Answer1", "Answer2");
+        var question = new Question("Question", List.of(new Answer("Answer1", false), new Answer("Answer2", false)));
+
+        String actualQuestionFormatedText = converter.toString(question);
+
+        assertThat(actualQuestionFormatedText).isEqualTo(expectedQuestionFormatedText);
+    }
+}
