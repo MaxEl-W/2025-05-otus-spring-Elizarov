7d75ec6ef2072122fc218d5fde4de18829c25e41
m.elizarov <maxel5g@gmail.com>
2025-11-06 08:22:03 +0300

    UPD messages for AnswerReader

--- hw03/src/main/java/ru/otus/hw/service/StreamsIOAnswerReaderImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/StreamsIOAnswerReaderImpl.java
@@ -15,14 +15,13 @@ public class StreamsIOAnswerReaderImpl implements AnswerReader {
     public Answer readUserChoice(Question question) {
         int maxAnswerNumber = question.answers().size() + ANSWER_START_NUMBER - 1;
 
-        //TODO перевод
         try {
-            int userChoice = ioService.readIntForRangeWithPrompt(ANSWER_START_NUMBER, maxAnswerNumber, "Choose right answer:",
-                    "The answer was entered incorrectly");
+            int userChoice = ioService.readIntForRangeWithPromptLocalized(ANSWER_START_NUMBER, maxAnswerNumber,
+                    "AnswerReader.input.right.answer", "AnswerReader.wrong.answer");
             int index = userChoice - ANSWER_START_NUMBER;
             return question.answers().get(index);
         } catch (IllegalArgumentException e) {
-            ioService.printLine("The number of attempts has been exhausted. The answer is not counted.\n");
+            ioService.printLineLocalized("AnswerReader.attempts.exhausted");
             return null;
         }
     }
