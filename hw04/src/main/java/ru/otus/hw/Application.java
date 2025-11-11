77fcb14382cfeb6c50ce8b16b508cca5573c6f0a
m.elizarov <maxel5g@gmail.com>
2025-11-12 00:20:06 +0300

    ADD: shell

--- hw04/src/main/java/ru/otus/hw/Application.java
+++ hw04/src/main/java/ru/otus/hw/Application.java
@@ -10,10 +10,6 @@ import ru.otus.hw.service.TestRunnerService;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
-        var context = SpringApplication.run(Application.class, args);
-        var testRunnerService = context.getBean(TestRunnerService.class);
-        testRunnerService.run();
-
+        SpringApplication.run(Application.class, args);
     }
 }
