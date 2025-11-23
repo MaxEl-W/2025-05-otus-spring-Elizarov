437218c7171197afd657881805a9ef91c538fab0
m.elizarov <maxel5g@gmail.com>
2025-11-24 01:13:48 +0300

    ADD tests

--- hw06/src/test/java/ru/otus/hw/repositories/JdbcGenreRepositoryTest.java
+++ hw06/src/test/java/ru/otus/hw/repositories/JdbcGenreRepositoryTest.java
@@ -3,8 +3,6 @@ package ru.otus.hw.repositories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
@@ -16,7 +14,7 @@ import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("Репозиторий на основе Jdbc для работы с авторами ")
+@DisplayName("Репозиторий на основе Jdbc для работы с жанрами ")
 @JdbcTest
 @Import({JdbcGenreRepository.class})
 class JdbcGenreRepositoryTest {
