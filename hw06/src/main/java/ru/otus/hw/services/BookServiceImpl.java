5c705d84a695c834e9fe13b822a2f17de9740563
m.elizarov <maxel5g@gmail.com>
2025-11-24 01:12:52 +0300

    UPD services

--- hw06/src/main/java/ru/otus/hw/services/BookServiceImpl.java
+++ hw06/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -1,11 +1,14 @@
 package ru.otus.hw.services;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.repositories.AuthorRepository;
 import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
 import ru.otus.hw.repositories.GenreRepository;
 
 import java.util.List;
@@ -23,22 +26,43 @@ public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
 
+    private final CommentRepository commentRepository;
+
     @Override
+    @Transactional(readOnly = true)
     public Optional<Book> findById(long id) {
-        return bookRepository.findById(id);
+        return bookRepository.findById(id).map(this::initializeAllLazyFields);
+    }
+
+    private Book initializeAllLazyFields(Book book) {
+        book.getAuthor().getFullName();
+        initializeLazyCollections(book);
+        return book;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Book> findAll() {
-        return bookRepository.findAll();
+        var books = bookRepository.findAll();
+        if (!books.isEmpty()) {
+            initializeLazyCollections(books.get(0));
+        }
+        return books;
+    }
+
+    private void initializeLazyCollections(Book book) {
+        book.getComments().size();
+        book.getGenres().size();
     }
 
     @Override
+    @Transactional
     public Book insert(String title, long authorId, Set<Long> genresIds) {
         return save(0, title, authorId, genresIds);
     }
 
     @Override
+    @Transactional
     public Book update(long id, String title, long authorId, Set<Long> genresIds) {
         return save(id, title, authorId, genresIds);
     }
---
+++ hw06/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
@@ -0,0 +1,49 @@
+package ru.otus.hw.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private BookRepository bookRepository;
+
+    private CommentRepository commentRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Comment> findById(long id) {
+        return commentRepository.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Comment> findByBook(long bookId) {
+        return commentRepository.findByBook(bookId);
+    }
+
+    @Override
+    @Transactional
+    public void addComment(long bookId, String commentText) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException(("Book with id %d not found").formatted(bookId)));
+        var comment = new Comment(0, book, commentText);
+        book.getComments().add(comment);
+        bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteComment(long commentId) {
+        commentRepository.deleteById(commentId);
+    }
+}
